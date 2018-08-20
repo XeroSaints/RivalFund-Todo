167d826b0978fd4f0c19c2ec02bca6280d8d9408
Xavier Santos <xerosaints@yahoo.com>
2018-08-19 23:32:18 -0400

    Convert to MVC design pattern

---
+++ RivalFund Todo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  RivalFund Todo
+//
+//  Created by X on 8/19/18.
+//  Copyright © 2018 Xavier Santos. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
